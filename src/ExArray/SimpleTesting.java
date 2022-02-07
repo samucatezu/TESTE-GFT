b25d5185aa3edadefff07e8b781aab644a9211f7
Samuel Almeida <barbosasamuel24@gmail.com>
2022-02-07 18:11:26 -0300

    ExArray finished

--- src/ExArray/SimpleTesting.java
+++ src/ExArray/SimpleTesting.java
@@ -3,9 +3,10 @@ import java.util.*;
 
 public class SimpleTesting{
     public static void main(String[] args) {
-        int arr[] = new int[] {12,34,45,21,33,4};
+        int arr[] = new int[] {12,34,45,21,36,4};
         int sum = 0;
 
+        double m=0;
         SimpleTesting obj = new SimpleTesting();
         if(arr == null) {
             System.out.println("The array is null");
@@ -14,12 +15,13 @@ public class SimpleTesting{
         else for(int i = 0; i < arr.length; i++) {
             sum+=arr[i];
             Arrays.sort(arr);
-
-
         }
+
+
         System.out.println("Menor numero = " + arr[0]);
         System.out.println("Maior numero = " + arr[arr.length-1]);
         System.out.println("Soma de todos os numeros do array = "+sum);
+       
 
 
     }
